013d27e4e2fc4764f72c7d4e25044d02e643cbac
swinarah <swinarah@l7systems.net>
2019-01-22 09:01:59 +0700

    Alarm Manager finished

---
+++ app/src/main/java/com/javasoul/alarmmanager/AlarmReceiver.java
@@ -0,0 +1,25 @@
+package com.javasoul.alarmmanager;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+import android.widget.Toast;
+
+public class AlarmReceiver extends WakefulBroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "ALARM!! ALARM!!", Toast.LENGTH_SHORT).show();
+
+        //Stop sound service to play sound for alarm
+        context.startService(new Intent(context, AlarmSoundService.class));
+
+        //This will send a notification message and show notification in notification tray
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                AlarmNotificationService.class.getName());
+        startWakefulService(context, (intent.setComponent(comp)));
+
+    }
+
+}
